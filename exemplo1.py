27e69a8a5a2d751348ea0202466e6273b496d0f3
DaniloM98spec <batistademiranda@gmail.com>
2025-03-14 19:44:10 -0300

    primeiro exemplo

---
+++ exemplo1.py
@@ -0,0 +1,8 @@
+a = input("Entre com o seu nome:")
+b = input("Entre com a sua idade:")
+print("O seu nome é" , a, " e a sua iade é", b)
+if(b > 18):
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+    
